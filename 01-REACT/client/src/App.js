4533ed19c3b4f257fcf93b3124916045ed66e9f8
nathansabin <sabinnat001@gmail.com>
2023-09-05 22:23:50 -0700

    started example REACT app

---
+++ 01-REACT/client/src/App.js
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+import Header from "./components/header/header"
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 01-REACT/client/src/components/header/header.js
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+import './header.css';
+
+const Header = () => {
+
+    return (
+        <div>
+            <div className={"banner"}>
+                <h1 className={"header"}>Hello Programmer</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
