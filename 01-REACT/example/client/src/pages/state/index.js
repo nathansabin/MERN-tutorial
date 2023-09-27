bc5fed4a5e24bb4b97cd5bf93ac188b0fac41bd8
nathansabin <sabinnat001@gmail.com>
2023-09-26 18:44:19 -0700

    finished style on use effect and use State

--- 01-REACT/example/client/src/pages/state/index.js
+++ 01-REACT/example/client/src/pages/state/index.js
@@ -1,4 +1,5 @@
 import React, {useState}from 'react';
+import './useState.css';
 
 function State(props) {
     const [count, setCount] = useState(0);
@@ -12,13 +13,17 @@ function State(props) {
     }
 
     return (
-        <div>
-            <h2>UseState</h2>
-            <p></p>
-            <div>
-                <h3>{count}</h3>
-                <button onClick={addCount}>Add One</button>
-                <button onClick ={clearCount}>Clear count</button>
+        <div className={'effect-page'}>
+            <div className={'container'}>
+                <h2>UseState</h2>
+                <p></p>
+                <div>
+                    <h3>{count}</h3>
+                    <div className={'buttons'}> 
+                        <button onClick={addCount}>Add One</button>
+                        <button onClick ={clearCount}>Clear count</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
