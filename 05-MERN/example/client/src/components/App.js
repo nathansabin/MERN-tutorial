23e55974dbfcdd4ebf2087cd07d78c0c64fa26cf
nathansabin <sabinnat001@gmail.com>
2023-11-04 19:19:24 -0700

    set up react routing

--- 05-MERN/example/client/src/components/App.js
+++ 05-MERN/example/client/src/components/App.js
@@ -1,11 +1,43 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './header';
 import Footer from './footer';
+import {
+  Home,
+  Login,
+  Buy,
+  Sell,
+  History
+} from "../pages";
 
 function App() {
   return (
     <div className="App">
       <Header />
+        <Router>
+          <Routes>
+            <Route
+              path="/login"
+              element={<Login/>}
+            />
+            <Route
+              path="/home"
+              element={<Home/>}
+            />
+            <Route
+              path="/buy"
+              element={<Buy/>}
+            />
+            <Route
+              path="/sell"
+              element={<Sell/>}
+            />
+            <Route
+              path="/history"
+              element={<History/>}
+            />
+          </Routes>
+        </Router>
       <Footer />
     </div>
   );
---
+++ 05-MERN/example/client/src/pages/index.js
@@ -0,0 +1,13 @@
+import Home from "./home";
+import Login from "./login";
+import Buy from "./buy";
+import Sell from "./sell";
+import History from "./history";
+
+export {
+    Home,
+    Login,
+    Buy,
+    Sell,
+    History
+}
