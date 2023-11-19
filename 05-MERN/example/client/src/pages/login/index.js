155a17d67c77b296beff3c9fd94bf431215d51d0
nathansabin <sabinnat001@gmail.com>
2023-11-18 17:49:51 -0800

    added login ui

--- 05-MERN/example/client/src/pages/login/index.js
+++ 05-MERN/example/client/src/pages/login/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import "./login.css";
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function Login() {
     return (
-        <div>
-            <h1>Login</h1>
+        <div className='login mt-3'>
+        <div className='container'>
+            <h1 className='col-12'>Login</h1>
+            <form className='col col-vert'>
+                <input type="text" className='col-12 col-lg-8 mb-2'></input>
+                <input type="text" className='col-12 col-lg-8 mb-3'></input>
+                <input type="submit" className='col-8 col-lg-6 btn btn-primary button'></input>
+            </form>
+        </div>
         </div>
     );
 }
-
+ 
 export default Login;
